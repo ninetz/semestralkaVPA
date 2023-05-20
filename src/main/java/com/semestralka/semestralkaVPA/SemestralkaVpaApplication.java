cfccf86518959791b366445f573a555320a84827
setlater <setlater@y.com>
2023-05-20 12:47:48 +0200

    First commit

---
+++ src/main/java/com/semestralka/semestralkaVPA/SemestralkaVpaApplication.java
@@ -0,0 +1,13 @@
+package com.semestralka.semestralkaVPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralkaVpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralkaVpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semestralka/semestralkaVPA/SemestralkaVpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semestralka.semestralkaVPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemestralkaVpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
