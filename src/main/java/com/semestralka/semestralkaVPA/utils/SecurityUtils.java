19b7c9f8d0fccd0b94a9a6350b8e407322122f31
setlater <setlater@y.com>
2023-05-21 20:05:55 +0200

    Add SecurityUtils - common security checks

---
+++ src/main/java/com/semestralka/semestralkaVPA/utils/SecurityUtils.java
@@ -0,0 +1,20 @@
+package com.semestralka.semestralkaVPA.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+public class SecurityUtils {
+    public static boolean isUserAnonymous(Authentication auth) {
+        return auth.getPrincipal().toString().toLowerCase().contains("anon".toLowerCase());
+    }
+
+    public static boolean isUserInRole(Authentication auth, String role) {
+        for (GrantedAuthority grantedAuthority : auth.getAuthorities()
+        ) {
+            if (grantedAuthority.getAuthority().equals(role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
